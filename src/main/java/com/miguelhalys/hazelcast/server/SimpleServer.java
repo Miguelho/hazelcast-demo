ca502ad50d0deb889829f9f24f534dd0cacfb534
Miguel Halys Ortuño <mhalys91@gmail.com>
2018-01-31 21:05:28 +0100

    init commit

---
+++ src/main/java/com/miguelhalys/hazelcast/server/SimpleServer.java
@@ -0,0 +1,13 @@
+package com.miguelhalys.hazelcast.server;
+
+import com.hazelcast.core.Hazelcast;
+import com.hazelcast.core.HazelcastInstance;
+
+public class SimpleServer {
+
+	public static void main(String[] args) {
+		HazelcastInstance hazelcastInstance = Hazelcast.newHazelcastInstance();
+
+	}
+
+}
